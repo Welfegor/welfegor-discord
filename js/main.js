eafe68ca79b35595c333e43e31c5e39be6ee4277
Welfegor <46806143+Welfegor@users.noreply.github.com>
2021-05-03 19:07:24 +1000

    Main upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.FAQ-button').click(function() {
+    $('.FAQ-content').slideToggle();
+  });
+  $('.DEV-button').click(function() {
+    $('.DEV-content').slideToggle();
+  });
+});
